5d384d8f983cf0bffffd0fc5f8a2cbe5d3327060
norival marson junior <jrmarson.nmj@gmail.com>
2020-09-23 21:07:21 -0300

    H2 database, test profile, JPA

--- src/main/java/com/webwordmarson/curso/entities/User.java
+++ src/main/java/com/webwordmarson/curso/entities/User.java
@@ -2,6 +2,15 @@ package com.webwordmarson.curso.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+
+@Entity
 public class User implements Serializable {
 	
 	/**
@@ -9,6 +18,9 @@ public class User implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
