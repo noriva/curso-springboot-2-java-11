6b25e117c165abd0e4749afab178034836e1ec30
norival marson junior <jrmarson.nmj@gmail.com>
2020-10-05 21:10:42 -0300

    Emum order status

--- src/main/java/com/webwordmarson/curso/entities/Order.java
+++ src/main/java/com/webwordmarson/curso/entities/Order.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
+import com.webwordmarson.curso.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,28 +22,29 @@ public class Order implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private User client;
-	
+
 	public Order() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -61,6 +62,19 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if(orderStatus != null) {
+			
+			this.orderStatus = orderStatus.getCode();
+		}
+		
 	}
 
 	public User getClient() {
@@ -95,7 +109,5 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
