08fa57d5ef63d7d47fff868044b39c3578a8d2ff
norival marson junior <jrmarson.nmj@gmail.com>
2020-10-10 16:00:44 -0300

    EXCEPTION HANDLING FIND BY ID

--- src/main/java/com/webwordmarson/curso/services/UserService.java
+++ src/main/java/com/webwordmarson/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.webwordmarson.curso.entities.User;
 import com.webwordmarson.curso.repositories.UserRepository;
+import com.webwordmarson.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
